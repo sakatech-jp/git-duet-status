d3425535a134e25e4c571901443c13a5a551e364
GitHub Action <action@github.com>
2022-07-21 08:45:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/sakatechjp/gitduetstatus/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sakatechjp/gitduetstatus/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sakatechjp.gitduetstatus.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sakatechjp.gitduetstatus.MyBundle
 
 class MyApplicationService {
 
